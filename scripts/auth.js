fe500e25a581a0dd5a1e6fe2c70f5417c6dcaddb
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-22 12:17:42 +0100

    Logging in Users

--- scripts/auth.js
+++ scripts/auth.js
@@ -35,3 +35,26 @@ logout.addEventListener('click', (e) => {
       console.log(er);
     });
 });
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((cred) => {
+      console.log(cred.user);
+      //close login modal and reset form
+      const modal = document.querySelector('#modal-login');
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
